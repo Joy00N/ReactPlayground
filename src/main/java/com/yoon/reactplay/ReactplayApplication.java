a479e2a90901459632e67323e55e2d008dd9a49b
Joy <yoonhee.jo.cs@gmail.com>
2019-02-20 23:49:46 -0500

    initial setting

---
+++ src/main/java/com/yoon/reactplay/ReactplayApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.reactplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactplayApplication.class, args);
+	}
+
+}
