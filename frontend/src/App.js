6810447ef36ed73ecd72716f7c0e59b3f672ddbf
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-03-06 01:51:50 -0500

    merge commit
    merge commit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,6 @@ const testData = [
     {name: "Joy", avatar_url: "https://avatars3.githubusercontent.com/u/38506754?v=4", company: "1234!"},
     {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
     {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-
 ];
 
 const CardList = (props) => (
